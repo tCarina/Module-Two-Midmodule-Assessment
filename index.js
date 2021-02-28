6ccc0258e6092d45c057c97569968488d0a96409
Cari <t.carina@rocketmail.com>
2021-02-28 13:26:19 -0500

    Get correct num of dice to diplay

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const dice = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"]
-const length = document.querySelector("dice-length")
-const submit = document.querySelector("dice-submit")
-const diceOutput = document.querySelector("dice-para")
-const sum = document.querySelector("sum-para")
-const hisOutput = document.querySelector("dice-history")
+
+const dice = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"];
+const form = document.querySelector("#dice-form");
+const submit = document.querySelector("#dice-submit");
+let diceOutput = document.querySelector("#dice-para");
+
+const rollDice = (event) => {
+  event.preventDefault();
+  diceOutput.textContent = ""
+  const num = document.querySelector("#dice-length").value;
+  const diceNum = parseInt(num);
+  for (let i = 0; i < diceNum; i++) {
+    const ranDice = Math.floor(Math.random() * dice.length);
+    diceOutput.textContent += dice[ranDice];
+  }
+};
+
+form.addEventListener("submit", rollDice);
+let sum = document.querySelector("#sum-para");
+let hisOutput = document.querySelector("#dice-history");
