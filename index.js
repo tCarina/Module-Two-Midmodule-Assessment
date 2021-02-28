bbfb91fb325d5bb905f9e36ff2469d9eea186eb1
Cari <t.carina@rocketmail.com>
2021-02-28 12:32:04 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const dice = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"]
+const length = document.querySelector("dice-length")
+const submit = document.querySelector("dice-submit")
+const diceOutput = document.querySelector("dice-para")
+const sum = document.querySelector("sum-para")
+const hisOutput = document.querySelector("dice-history")
